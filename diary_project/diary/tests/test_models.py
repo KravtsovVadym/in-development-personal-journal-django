cad3cd5aeccdf784265796792dd1762312d39e0a
vadym <vadymkravtcsov@gmail.com>
2025-11-05 00:23:13 +0200

    feat: add create test for the model

---
+++ diary_project/diary/tests/test_models.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from diary.models import Entry, Tag, User
+# Create your tests here.
+class EntryTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username="Donald")
+    
+    def test_cascade_delete(self):
+        Entry.objects.create(
+            author=self.user,
+            title="test",
+            content="Hello, this is a test for the Entry model"
+            )
+        self.user.delete()
+        self.assertEqual(Entry.objects.count(), 0)
+    
+    
+    def test_tags(self):
+        entry = Entry.objects.create(
+            author=self.user,
+            title="test",
+            content="Hello, it's me, now we check the tag table"
+            )
+        tag = Tag.objects.create(name="My first tag")
+        entry.tags.add(tag)
+        self.assertEqual(entry.tags.count(), 1)
+        self.assertIn(entry, tag.entries.all()) # type: ignore
